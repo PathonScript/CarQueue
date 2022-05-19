c5bcd8c9a143a3ff3abb8414eb9ae7bacaf7af8a
PathonScript <pathonscript@gmail.com>
2022-05-19 19:49:33 +0700

    updated link for new tba

--- src/lib/components/Socials.svelte
+++ src/lib/components/Socials.svelte
@@ -2,5 +2,9 @@
 	import SocialIcons from '@rodneylab/svelte-social-icons';
 </script>
 
-<SocialIcons network="github" fgColor="#eeeeee" bgColor="#111111" />
-<SocialIcons network="twitter" fgColor="#eeeeee" bgColor="#111111" />
+<a href="https://github.com/PathonScript" target="_blank">
+	<SocialIcons network="github" fgColor="#eeeeee" bgColor="#111111" />
+</a>
+<a href="https://twitter.com/PathonScript" target="_blank">
+	<SocialIcons network="twitter" fgColor="#eeeeee" bgColor="#111111" />
+</a>
