a33a25f7d56182ac7db1db30f2c3122088a5a6dd
PathonScript <pathonscript@gmail.com>
2022-06-08 05:59:33 +0700

    Status color

--- src/lib/components/Queue.svelte
+++ src/lib/components/Queue.svelte
@@ -2,12 +2,14 @@
   export let name;
   export let car;
   export let status;
+
+  import Status from './Status.svelte'
 </script>
 
 <div class="m-2">
 <div class="flex flex-col w-full items-center justify-center min-h-min p-4 border-4 border-gray rounded-lg">
     <h1 class="text-6xl">{name}</h1> 
     <h1 class="text-3xl">{car}</h1> 
-    <h1 class="text-3xl">{status}</h1> 
+    <Status status={status}/>
   </div>
 </div>
